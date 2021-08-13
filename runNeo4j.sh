2f895e55d70f501d683dc87f46a8521616c30615
Jennifer Reif <jennifer.reif@neo4j.com>
2021-08-13 16:07:31 -0500

    Update to use arm64 architecture

--- runNeo4j.sh
+++ runNeo4j.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-docker run --name myneo4j -p7474:7474 -p7687:7687 -d -v $HOME/Projects/docker/data/neo4j:/data -v $HOME/Projects/docker/logs/neo4j:/logs jmreif/neo4j
+docker run \
+    --name myneo4j -p7474:7474 -p7687:7687 \
+    -e NEO4J_AUTH=neo4j/Testing123 \
+    -d -v $HOME/Projects/docker/data/neo4j:/data \
+    -v $HOME/Projects/docker/logs/neo4j:/logs \
+    -v $HOME/Projects/docker/data/neo4j/import:/import \
+    -v $HOME/Projects/docker/data/neo4j/plugins:/plugins \
+    jmreif/neo4j-arm64
 docker ps
