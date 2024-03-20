90c9ccd866aed207106a22b684bc81693f063786
Jennifer Reif <jennifer.reif@neo4j.com>
2024-03-20 15:46:53 -0500

    Update with latest neo4j

--- runNeo4j.sh
+++ runNeo4j.sh
@@ -3,6 +3,7 @@
 docker run \
     --name myneo4j -p7474:7474 -p7687:7687 \
     -e NEO4J_AUTH=neo4j/Testing123 \
+    -e NEO4J_PLUGINS='["apoc"]' \
     -d -v $HOME/Projects/docker/data/neo4j:/data \
     -v $HOME/Projects/docker/logs/neo4j:/logs \
     -v $HOME/Projects/docker/data/neo4j/import:/import \
